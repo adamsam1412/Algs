2edbfd2e1b783fb8eb585635e0cd740256ce3de3
Adam <adamsam1412@gmail.com>
2018-08-18 23:42:52 +0200

    Connected Components added

--- Graphs/Graph.cpp
+++ Graphs/Graph.cpp
@@ -1,9 +1,16 @@
+//To section all these algorithms off
 #include <iostream>
 #include <queue>
+#include <list>
 
 #define V 6
-
+#define E 10
 using namespace std;
+
+struct G{
+    int head[V+1];
+    int next[2E];
+};
 
 bool isBipartite(int G[][V], int src)
 {
@@ -37,5 +44,35 @@ bool isBipartite(int G[][V], int src)
     }
     return true;
 }
+
+int connectedComponents(G& graph)
+{
+    int colors[V];
+    
+    for(auto i = 0; i < V; ++i)
+        colors[i] = -1;
+
+    int count = 0;
+    for(auto i = 0; i < V; ++i)
+    {
+        if(colors[i] == -1)
+        {
+            count++;
+
+        }
+    }
+
+}
+
+void dfsVisit(G & graph, int colors[], int current)
+{
+    colors[current] = 1;
+    for(auto i = graph.head[current]; i < graph.head[current+1]; ++i)
+    {
+        if(colors[graph.next[i]] == -1)
+            dfsVisit(graph, colors, graph.next[i]);
+    }
+}
+
 
 
